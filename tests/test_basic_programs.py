da98722a7f5e94099f15fa392469beac3c7e2d10
Eric Matthes <ehmatthes@gmail.com>
2024-01-17 17:22:55 -0900

    Parametrized version of hello_world.py test.

--- tests/test_basic_programs.py
+++ tests/test_basic_programs.py
@@ -2,11 +2,21 @@ import subprocess, sys
 from pathlib import Path
 from shlex import split
 
+import pytest
 
-def test_basic_program():
+
+import pytest
+
+basic_programs = [
+    ('chapter_01/hello_world.py', 'Hello Python world!'),
+]
+
+@pytest.mark.parametrize(
+    'file_path, expected_output', basic_programs)
+def test_basic_program(file_path, expected_output):
     """Test a program that only prints output."""
     root_dir = Path(__file__).parents[1]
-    path = root_dir / "chapter_01" / "hello_world.py"
+    path = root_dir / file_path
 
     # Use the venv python.
     python_cmd = f"{sys.prefix}/bin/python"
@@ -18,4 +28,4 @@ def test_basic_program():
         capture_output=True, text=True, check=True)
     output = result.stdout.strip()
 
-    assert output == "Hello Python world!"
+    assert output == expected_output
