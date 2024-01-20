d01283e2d5bc188e8d0b648dd3a3f9f419a84b44
Eric Matthes <ehmatthes@gmail.com>
2024-01-20 13:13:25 -0900

    Shows which version of Python was used to run tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,4 +11,12 @@ def python_cmd():
     """Return path to the venv Python interpreter."""
     return utils.get_python_cmd()
 
+def pytest_sessionfinish(session, exitstatus):
+    """Custom cleanup work."""
 
+    # Show which version of Python was used for tests.
+    python_cmd = utils.get_python_cmd()
+    cmd = f"{python_cmd} --version"
+    output = utils.run_command(cmd)
+
+    print(f"***** Tests were run with: {output}")
