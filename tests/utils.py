a8908e659f8ee5aa25a92907f3f33279e1a67c26
Eric Matthes <ehmatthes@gmail.com>
2024-01-21 13:12:05 -0900

    Utility function for checking library versions.

--- tests/utils.py
+++ tests/utils.py
@@ -19,3 +19,22 @@ def get_python_cmd():
         python_cmd = Path(sys.prefix) / "bin/python"
 
     return python_cmd.as_posix()
+
+def check_library_version(request, python_cmd, lib_name):
+    """Install a specific version of a library if needed."""
+    lib_version = request.config.getoption(f"--{lib_name}-version")
+
+    if lib_version:
+        print(f"\n*** Installing {lib_name} {lib_version}\n")
+        cmd = f"{python_cmd} -m pip install {lib_name}=={lib_version}"
+        output = run_command(cmd)
+        print(output)
+
+    # Regardless of what version was requested,
+    #   show which version is being used.
+    cmd = f"{python_cmd} -m pip freeze | grep {lib_name}"
+    result = subprocess.run(cmd, capture_output=True,
+            text=True, check=True, shell=True)
+    output = result.stdout.strip()
+
+    print(f"\n*** Running tests with {output}\n")
