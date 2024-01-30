001fe9fc2d3634a7ae5d1c1aef5fd2b15330dc35
Eric Matthes <ehmatthes@gmail.com>
2024-01-29 17:27:46 -0900

    First mpl test modifies original mpl file.

---
+++ tests/test_matplotlib_programs.py
@@ -0,0 +1,32 @@
+"""Test all Matplotlib programs.
+
+Chapter 15: Basic plots, random walks
+Chapter 16: Weather plots
+
+Overall approach:
+- Copy code to a tmp dir.
+- Modify code to call savefig() instead of plt.show().
+- Compare output files against reference files.
+
+"""
+
+from pathlib import Path
+import shutil
+
+
+def test_mpl_squares(tmp_path, python_cmd):
+    # Copy program file to temp dir.
+    test_file = "chapter_15/plotting_simple_line_graph/mpl_squares.py"
+    src_path = Path(__file__).parents[1] / test_file
+
+    dest_path = tmp_path / src_path.name
+    shutil.copy(src_path, dest_path)
+
+    # Replace plt.show() with savefig().
+    contents = dest_path.read_text()
+    save_cmd = 'plt.savefig("output_file.png")'
+    contents = contents.replace("plt.show()", save_cmd)
+    dest_path.write_text(contents)
+
+    print(f"\n***** Modified file: {dest_path}")
+
